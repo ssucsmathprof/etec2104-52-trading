362842d2926934121412fedd390b3881b2991577
John Reed <theredcoat787@outlook.com>
2026-03-17 19:31:50 -0400

    create basic TraderBot class and auto_import function

--- tradebot.py
+++ tradebot.py
@@ -2,46 +2,77 @@
 import requests
 import json
 import sys
+import csv
 
 BASE_URL = "http://127.0.0.1:8000/api/orders"
-USERNAME = "admin"
-PASSWORD = "admin"
 
-def place_order(symbol, price, side, quantity):
-    payload = {
-        "symbol": symbol,
-        "price": price,
-        "side": side,
-        "quantity": quantity
-    }
+class TraderBot:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
-    print("Sending order:")
-    print(json.dumps(payload, indent=4))
+    def place_order(self, symbol, price, side, quantity):
+        payload = {
+            "symbol": symbol,
+            "price": price,
+            "side": side,
+            "quantity": quantity
+        }
 
-    response = requests.post(
-        BASE_URL,
-        json=payload,
-        auth=(USERNAME, PASSWORD),
-    )
+        print("Sending order:")
+        print(json.dumps(payload, indent=4))
 
-    print("\nStatus Code:", response.status_code)
+        response = requests.post(
+            BASE_URL,
+            json=payload,
+            auth=(self.username, self.password),
+        )
 
-    try:
-        print(json.dumps(response.json(), indent=4))
-    except Exception:
-        print(response.text)
+        print("\nStatus Code:", response.status_code)
+
+        try:
+            print(json.dumps(response.json(), indent=4))
+        except Exception:
+            print(response.text)
+
+    def get_orders(self):
+        response = requests.get(
+            url=BASE_URL + "/open",
+            auth=(self.username, self.password),
+        )
+        return response.json() # just raw list[dict[]]
+
+type TeamName = str
+def automatic_input(file_path, traderbots: dict[TeamName, TraderBot]):
+    """Place orders from csv file at `file_path`, example:
+    ```file_path.csv
+    team,symbol,price,side,quantity
+    "teamA","BEAR",10,"BUY",100
+    ```
+    """
+    with open(file_path, newline='') as file:
+        reader = csv.DictReader(file)
+        for order_num,order_record in enumerate(reader):
+            traderbot = traderbots.get(order_record["team"])
+            if traderbot is None:
+                print(f'Order #{order_num}: {order_record}')
+                print(f'\tUnknown Trader: {order_record["team"]}')
+                continue
+
+            traderbot.place_order(order_record["symbol"], order_record["price"],
+                                  order_record["side"], order_record["quantity"])
+
 
 if __name__ == "__main__":
 
-    symbol = "BEAR"
-    price = 101
-    side = "SELL"
-    quantity = 10
+    traderbots = {
+        "teamA": TraderBot("teamA","team1234"),
+        "teamB": TraderBot("teamB","team1234"),
+    }
 
-    if len(sys.argv) == 5:
-        symbol = sys.argv[1]
-        price = float(sys.argv[2])
-        side = sys.argv[3].upper()
-        quantity = int(sys.argv[4])
+    for traderbot in traderbots.values():
+        traderbot.get_orders()
 
-    place_order(symbol, price, side, quantity)
+    exit()
+    automatic_input("sample-auto-input.csv", traderbots)
+    exit()
