678a7a57f42e759962524b952ef4ba9e1d21e041
AestheticYellow <kfkouba5231@gmail.com>
2026-03-24 19:17:41 -0400

    added stuff to randombot

--- trading/strategy/randomBot.py
+++ trading/strategy/randomBot.py
@@ -1,40 +1,44 @@
 import random
-from str_universal import can_buy,place_order
+from str_universal import *
 
 
 class randomBot():
     def __init__(self, trader):
-        mood = 0
+        mood = 0 #this is basically just what makes choices
         self.trader = trader
 
 
     def doStuff(self,mood):
-        orders = []
-        # random bs, go!
+        mood = random.random()
+        # buy,buy,buy!
         if mood < 0.5:
-            spread = random.randint(1, 3)
-            size = random.randint(5, 20)
+            side = "BUY"
+            qty = random.randint(10, 40)
 
             if can_buy(self.trader):
-                place_order(self.trader)
+                place_order(self.trader.user.username,
+                            symbol,price,side,qty)
 
         # impulse
         if mood < 0.8:
             side = random.choice(["BUY", "SELL"])
             qty = random.randint(1, 25)
             if side == "BUY" and can_buy(self.trader):
-                ...
+                place_order(self.trader.user.username,
+                            symbol, price,"BUY", qty)
             if side == "SELL":
-                ...
+                place_order(self.trader.user.username,
+                            symbol, price,"SELL", qty)
 
-        # Panik
+        # Smart
         else:
             side = random.choice(["BUY", "SELL"])
             qty = random.randint(10, 40)
 
             if side == "BUY":
                 if can_buy(self.trader):
-                    ...
-            else:
-                ...
-                #sell
+                    place_order(self.trader.user.username,
+                                symbol, price, side, qty)
+            if side == "SELL":
+                place_order(self.trader.user.username,
+                            symbol,price,side,qty)
