9168eab090075d0e6f4c0ebedf523cdf868299ef
AestheticYellow <kfkouba5231@gmail.com>
2026-03-26 19:03:57 -0400

    fleshed out strats in randomBot better

--- trading/strategy/randomBot.py
+++ trading/strategy/randomBot.py
@@ -3,42 +3,44 @@ from str_universal import *
 
 
 class randomBot():
-    def __init__(self, trader):
-        mood = 0 #this is basically just what makes choices
-        self.trader = trader
+    def __init__(self, traderName):
+        self.trader = traderName
+        self.mood = random.random() #this is basically just what makes choices
 
 
-    def doStuff(self,mood):
-        mood = random.random()
+    def doStuff(self,symbol,price,orderbook):
+        self.mood = random.random()
         # buy,buy,buy!
-        if mood < 0.5:
+        if self.mood < 0.4:
             side = "BUY"
-            qty = random.randint(10, 40)
-
-            if can_buy(self.trader):
-                place_order(self.trader.user.username,
+            qty = random.randint(1, 100)
+            print("BUY BUY BUY!!!!!")
+            if can_buy(self.trader) and side == "BUY":
+                place_order(self.trader,
                             symbol,price,side,qty)
 
-        # impulse
-        if mood < 0.8:
-            side = random.choice(["BUY", "SELL"])
+        # impulse sell
+        if self.mood > 0.4 and self.mood < 0.89:
+            side = "SELL"
             qty = random.randint(1, 25)
-            if side == "BUY" and can_buy(self.trader):
-                place_order(self.trader.user.username,
-                            symbol, price,"BUY", qty)
+            print("I am selling because lol. lmao even.")
             if side == "SELL":
-                place_order(self.trader.user.username,
-                            symbol, price,"SELL", qty)
+                place_order(self.trader,
+                            symbol,price,"SELL",qty)
 
-        # Smart
+        # Smart... maybe
         else:
-            side = random.choice(["BUY", "SELL"])
-            qty = random.randint(10, 40)
-
-            if side == "BUY":
-                if can_buy(self.trader):
-                    place_order(self.trader.user.username,
-                                symbol, price, side, qty)
-            if side == "SELL":
-                place_order(self.trader.user.username,
-                            symbol,price,side,qty)
+            qty = random.randint(1, 40)
+            orderMoney = orderbook.get_youngest_order()
+            print("I was never book smart im money smart, makes me more intel- more intelligent.\n"
+                  "Call me mister rock festival i got hella bands,\n shawty cute her circle too tell her get a friend")
+            if orderMoney < get_recent_orders(self.trader):
+                side = "BUY"
+                if can_buy(self.trader) and side == "BUY":
+                    place_order(self.trader,
+                                symbol,price,side,qty)
+            elif orderMoney >= get_recent_orders(self.trader):
+                side = "SELL"
+                if side == "SELL":
+                    place_order(self.trader,
+                                symbol,price,side,qty)
