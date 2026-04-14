5841f8067205e946e1d2bdc00628689c88be0f84
Bailey White <whiteb11@mymail.shawnee.edu>
2026-04-14 18:54:12 -0400

    Possibly final update for TrendFollower: Made TrendFollower into class. Also made RandomBot with TradeBot class.

--- trading/strategy/randomtradebot.py
+++ trading/strategy/randomtradebot.py
@@ -25,46 +25,55 @@ TRADE_COUNT = 1000
 # How often TradeBot runs; trades every TRADE_FREQUENCY seconds
 TRADE_FREQUENCY = 5
 
-random_bot = TradeBot(
-    username="RandomBot",
-    password="team1234"
-)
+class RandomBot:
+    def __init__(self, username, password):
+        self.bot = TradeBot(username=username, password=password)
+        self.symbols_list = []
+        self.market_report = self.bot.get_market()
+        logger.info(f"Created Random Bot: {self.bot.username}")
 
-# Generate list of symbols
-symbols_list = []
-market_report = random_bot.get_market()
-for report in market_report:
-    symbol = report.symbol.code
-    symbols_list.append(symbol)
+    def generate_symbol_list(self):
+        logger.debug("Generates the list of all symbols in the market")
+        for report in self.market_report:
+            symbol = report.symbol.code
+            self.symbols_list.append(symbol)
 
-def get_random_symbol():
-    logger.debug("Randomly picks a symbol from the list of symbols")
-    random_index = random.randint(0, len(symbols_list)-1)
-    return symbols_list[random_index]
+    def get_random_symbol(self):
+        logger.debug("Randomly picks a symbol from the list of symbols")
+        random_index = random.randint(0, len(self.symbols_list) - 1)
+        return self.symbols_list[random_index]
 
-def get_random_side():
-    logger.debug("Randomly picks a side from either BUY or SELL")
-    sides = ["BUY","SELL"]
-    random_index = random.randint(0, 1)
-    return sides[random_index]
+    def get_random_side(self):
+        logger.debug("Randomly picks a side from either BUY or SELL")
+        sides = ["BUY", "SELL"]
+        random_index = random.randint(0, 1)
+        return sides[random_index]
 
-def place_random_order():
-    logger.debug("Places a random order with a random symbol, price, side, and quantity")
-    random_price = random.randint(ORDER_MIN_PRICE, ORDER_MAX_PRICE)
-    random_quantity = random.randint(ORDER_MIN_QUANTITY, ORDER_MAX_QUANTITY)
-    random_bot.place_order(
-        symbol= get_random_symbol(),
-        price=random_price,
-        side= get_random_side(),
-        quantity=random_quantity
-    )
-    logger.info(f"Random TradeBot placed random order: {get_random_symbol(), random_price, get_random_side(), random_quantity}")
+    def place_random_order(self):
+        logger.debug("Places a random order with a random symbol, price, side, and quantity")
+        random_price = random.randint(ORDER_MIN_PRICE, ORDER_MAX_PRICE)
+        random_quantity = random.randint(ORDER_MIN_QUANTITY, ORDER_MAX_QUANTITY)
+        self.bot.place_order(
+            symbol=self.get_random_symbol(),
+            price=random_price,
+            side=self.get_random_side(),
+            quantity=random_quantity
+        )
+        logger.info(
+            f"Random TradeBot placed random order: {self.get_random_symbol(), random_price, self.get_random_side(), random_quantity}")
 
-def run_random_tradebot(num_ticks=TRADE_COUNT):
-    logger.debug("Runs random tradebot every")
-    for step in range(num_ticks):
-        place_random_order()
-        time.sleep(TRADE_FREQUENCY)
+    def run_random_tradebot(self, num_ticks=TRADE_COUNT):
+        logger.debug("Runs random tradebot every")
+        for step in range(num_ticks):
+            self.place_random_order()
+            time.sleep(TRADE_FREQUENCY)
 
-while True:
-    run_random_tradebot(num_ticks=TRADE_COUNT)
+def main():
+    randomBot = RandomBot("RandomBot", "team1234")
+    randomBot.generate_symbol_list()
+
+    while True:
+        randomBot.run_random_tradebot(num_ticks=TRADE_COUNT)
+
+if __name__ == "__main__":
+    main()
--- trading/strategy/trendfollower.py
+++ trading/strategy/trendfollower.py
@@ -9,96 +9,100 @@ Trend follower:
 
 logger = get_logger()
 
-trend_follower_bot = TradeBot(
-    username="TrendFollower",
-    password="team1234"
-)
-logger.info(f"Created Trend Follower Bot: {trend_follower_bot}")
+class TrendFollower:
+    def __init__(self, username, password):
+        self.bot = TradeBot(username=username, password=password)
+        self.price_history = {}
+        logger.info(f"Created TrendFollower Bot: {self.bot.username}")
 
-price_history = {} # Will store symbols and there 3 most recent trade prices
+    def update_price_history(self, market_report):
+        logger.debug('''Update price history: "
+                        Adds a symbol to price_history dict if it wasn't there. 
+                        Updates each symbol's last three prices by appending the latest price, and removing
+                        the oldest price.
+                        The price history of each symbol is kept to the last three prices''')
 
-def update_price_history(market_report):
-    logger.debug('''Update price history: "
-                    Adds a symbol to price_history dict if it wasn't there. 
-                    Updates each symbol's last three prices by appending the latest price, and removing
-                    the oldest price.
-                    The price history of each symbol is kept to the last three prices''')
+        for report in market_report:
+            symbol = report.symbol.code
+            logger.info(f"Updating price history for {symbol}")
+            price = report.last_price
 
-    for report in market_report:
-        symbol = report.symbol.code
-        logger.info(f"Updating price history for {symbol}")
-        price = report.last_price
+            if symbol not in self.price_history:
+                self.price_history[symbol] = []
 
-        if symbol not in price_history:
-            price_history[symbol] = []
+            self.price_history[symbol].append(price)
+            logger.info(f"Added {price} to {symbol} price history")
 
-        price_history[symbol].append(price)
-        logger.info(f"Added {price} to {symbol} price history")
+            if len(self.price_history[symbol]) > 3:
+                logger.info(f"Removing oldest price ({self.price_history[symbol][0]}) from {symbol} price history")
+                self.price_history[symbol].pop(0)
 
-        if len(price_history[symbol]) > 3:
-            logger.info(f"Removing oldest price ({price_history[symbol][0]}) from {symbol} price history")
-            price_history[symbol].pop(0)
+        logger.info(f"Updated price history: {self.price_history}")
 
-    logger.info(f"Updated price history: {price_history}")
+    def calculate_trend(self, symbol):
+        logger.debug('''Calculating price trend:
+                        Looks at each symbols three recent prices
+                        If there isn't three recent prices to look at, we don't have enough info so we wait.
+                        If there is an increase with each of the three prices->buy
+                        If there is an decrease with each of the three prices->sell
+                        If any of the latest prices equal or not ascending/descending, there is no trend to follower->wait''')
 
-def calculate_trend(symbol):
-    logger.debug('''Calculating price trend:
-                    Looks at each symbols three recent prices
-                    If there isn't three recent prices to look at, we don't have enough info so we wait.
-                    If there is an increase with each of the three prices->buy
-                    If there is an decrease with each of the three prices->sell
-                    If any of the latest prices equal or not ascending/descending, there is no trend to follower->wait''')
+        recent_prices = self.price_history.get(symbol, [])
+        logger.info(f"Calculating price trend for {symbol}")
+        logger.info(f"Recent prices: {recent_prices}")
 
-    recent_prices = price_history.get(symbol, [])
-    logger.info(f"Calculating price trend for {symbol}")
-    logger.info(f"Recent prices: {recent_prices}")
+        if len(recent_prices)< 3: # Not enough to see any trend
+            logger.info("Not enough information for trend")
+            return None
 
-    if len(recent_prices)< 3: # Not enough to see any trend
-        logger.info("Not enough information for trend")
-        return None
+        elif recent_prices[-3] < recent_prices[-2] < recent_prices[-1]: # Price trends rise
+            logger.info("Rising trend: Trend Follower biased to buy")
+            return "BUY"
+        elif recent_prices[-3] > recent_prices[-2] > recent_prices[-1]: # Price trends fall
+            logger.info("Falling trend: Trend Follower biased to sell")
+            return "SELL"
+        else: # No trend
+            logger.info("No trend calculated")
+            return "WAIT"
 
-    elif recent_prices[-3] < recent_prices[-2] < recent_prices[-1]: # Price trends rise
-        logger.info("Rising trend: Trend Follower biased to buy")
-        return "BUY"
-    elif recent_prices[-3] > recent_prices[-2] > recent_prices[-1]: # Price trends fall
-        logger.info("Falling trend: Trend Follower biased to sell")
-        return "SELL"
-    else: # No trend
-        logger.info("No trend calculated")
-        return "WAIT"
+    def market_decision(self):
+        logger.debug('''Deciding if Trend Follower will buy/sell:
+                        For each symbol, make a decision
+                        The decision is decided by calculate_trend
+                        If the decision is a BUY or SELL, TrendFollower will place and order''')
 
-def market_decision():
-    logger.debug('''Deciding if Trend Follower will buy/sell:
-                    For each symbol, make a decision
-                    The decision is decided by calculate_trend
-                    If the decision is a BUY or SELL, TrendFollower will place and order''')
+        market_report = self.bot.get_market()
+        self.update_price_history(market_report)
 
-    market_report = trend_follower_bot.get_market()
-    update_price_history(market_report)
+        for symbol in self.price_history:
+            decision = self.calculate_trend(symbol)
+            logger.info(f"{decision} at {symbol}")
 
-    for symbol in price_history:
-        decision = calculate_trend(symbol)
-        logger.info(f"{decision} at {symbol}")
+            if decision in ("BUY", "SELL"):
+                try:
+                    self.bot.place_order(
+                        symbol,
+                        self.price_history[symbol][2],
+                        decision,
+                        100
+                    )
+                    logger.info(f"Trend Follower places order: {symbol, self.price_history[symbol][2], decision, 100}")
+                except:
+                    logger.error(f"Trend Follower tried to place order: {symbol, self.price_history[symbol][2], decision, 100} and failed!")
 
-        if decision in ("BUY", "SELL"):
-            try:
-                trend_follower_bot.place_order(
-                    symbol,
-                    price_history[symbol][2],
-                    decision,
-                    100
-                )
-                logger.info(f"Trend Follower places order: {symbol, price_history[symbol][2], decision, 100}")
-            except:
-                logger.error(f"Trend Follower tried to place order: {symbol, price_history[symbol][2], decision, 100} and failed!")
-
-def run_trend_follower():
-    market_decision()
-    time.sleep(5)
+    def run_trend_follower(self):
+        logger.debug("Runs Trend Follower Bot by calling self.market_decision() and sleeping every 5 seconds")
+        self.market_decision()
+        logger.info(f"--------------------Running Trend Follower--------------------")
+        time.sleep(5)
 
 #TODO(done): Tradebot needs to look at each symbols trend, and buy/sell/wait on each
 #       symbol based off the calculated trend. (For each symbol, buy/sell/wait.)
+trend_follower = TrendFollower("TrendFollower", "team1234")
 
-#TODO(?): Run based off market ticks, and not seconds
-while True:
-    run_trend_follower()
+def main():
+    while True:
+        trend_follower.run_trend_follower()
+
+if __name__ == "__main__":
+    main()
